84ad67414057744502f71d51b29f698b5e58cdc4
break-stuff <burton@breakstuff.io>
2025-05-30 18:05:07 -0400

    update outdir

--- demo/generate-types.js
+++ demo/generate-types.js
@@ -2,7 +2,8 @@ import { generateJsxTypes } from '../dist/index.js';
 import manifest from './shoelace-cem.json' with { type: 'json' };
 
 generateJsxTypes(manifest, {
+  outdir: './demo/types',
   componentDescriptionOptions: {
-    descriptionSrc: 'summary'
+    descriptionSrc: 'summary',
   }
 });
